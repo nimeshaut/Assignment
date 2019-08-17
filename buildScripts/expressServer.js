637ce027801d8db91e0003e2aaec899b835e36be
nimeshaut <cyf0440@aut.ac.nz>
2019-08-18 05:11:19 +1200

    first introduction to react

--- buildScripts/expressServer.js
+++ buildScripts/expressServer.js
@@ -1,16 +1,49 @@
+// import config from '../config';
+// import apiRouter from '../api';
+
+// import express from 'express';
+// const server = express();
+
+// server.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
+
+// server.use('/api', apiRouter);
+// server.use(express.static('public'));
+
+// server.listen(config.port, (err) => {
+// //   console.info('Express listening on port', config.port);
+//     if (err) {
+//         console.log('an error occured while listening to port')
+//     } else {
+//         open('http://localhost:' + config.port);
+//     }
+// });
+
+import config from '../config'
+import apiRouter from '../api';
 var express = require('express'); // variable to hold express
 var path = require('path'); // variable to hold path
 var open = require('open'); // variable to hold open
 
-var port = 3000; // var to hold port from which express is going to use and serve
+var port = config.port; // var to hold port from which express is going to use and serve
 
 var app = new express(); // create instance of express
 
+app.set('view engine','ejs');
 // tell express which routes it is going to handle
-app.get('/', function (request, response) {
-    response.sendFile(path.join(__dirname, '../public/index.html'))
-});
+// app.get('/', function (request, response) {
+//     //response.sendFile(path.join(__dirname, '../public/index.html'))
+//     response.render('index');
+// });
+app.get('/', (req, res) => {
+    res.render('index', {
+      content: 'Hello Express and <em>EJS</em>'
+    });
+  });
 
+app.use('/api', apiRouter);
+app.use(express.static('public'));
 
 // now tell express to listen to port which we have defined
 
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// ReactDOM.render(
+//     React.createElement('h1', null, 'Assignment 1B'),
+//     document.getElementById('root')
+//   );
